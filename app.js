dfcd3d29c9c8fbcf5ea7f568075f4a7c3e418b7b
OnyinyeChristabel <onyinye241381@gmail.com>
2023-02-04 13:53:39 +0100

    change

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const hamburger = document.querySelector(".hide");
-const navigation = document.querySelector(".nav2")
+const navigation = document.querySelector(".nav2");
+const body = document.querySelector("body")
 
 hamburger.addEventListener("click", function () {
     if (hamburger.innerText === "☰") {
         hamburger.innerText = "✕"
         navigation.classList.add("open")
+        body.classList.add("noscroll")
     }
     else if (hamburger.innerText === "✕") {
         hamburger.innerText = "☰"
         navigation.classList.remove("open")
+        body.classList.remove("noscroll")
 
     }
 
